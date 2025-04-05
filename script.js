95afe4976e88d541e79c046356249f4e96dc9062
sateia-r <rinj2309@gmail.com>
2025-04-05 14:00:04 +0700

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-document.getElementById('kirim').addEventListener('click', (event) =>
-{
-
-event.preventDefault();
-let name = document.getElementById('name').value;
-let email = document.getElementById('email').value;
-let pesan = document.getElementById('pesan').value;
-
-const url = `https://wa.me/6285806528052?text=Nama: ${nama}%0AEmail: ${email}%0APesan: ${pesan}`;
-    window.open(url, '_blank');
+document.getElementById('kirim').addEventListener('click', (event) => {
+    event.preventDefault();
+    let nama = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let pesan = document.getElementById('pesan').value;
+    if (nama && email && pesan) {
+        const url = `https://wa.me/6285806528052?text=Nama: ${nama}%0AEmail: ${email}%0APesan: ${pesan}`;
+        window.open(url, '_blank');
+    } else {
+        alert("Silakan isi semua field");
+    }
 });
