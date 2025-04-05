194493d08d93bc34da1f0ff4353a224f3c688939
sateia-r <rinj2309@gmail.com>
2025-04-05 13:51:44 +0700

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById('kirim').addEventListener('click', (event) =>
 {
 
 event.preventDefault();
-const name = document.getElementById('name').value;
-const email = document.getElementById('email').value;
-const pesan = document.getElementById('pesan').value;
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let pesan = document.getElementById('pesan').value;
 
 const url = `https://wa.me/6285806528052?text=Nama: ${nama}%0AEmail: ${email}%0APesan: ${pesan}`;
     window.open(url, '_blank');
