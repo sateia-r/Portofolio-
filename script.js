5063a4b7b04d6b8fcf0f37cde166128c0b88094d
sateia-r <rinj2309@gmail.com>
2025-04-05 13:42:09 +0700

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const name = document.getElementById('name').value;
 const email = document.getElementById('email').value;
 const pesan = document.getElementById('pesan').value;
 
-const url = `https://wa.me/6285806528052? text=Nama: ${nama}%0AEmail: ${email}%0APesan: ${pesan}`;
+const url = `https://wa.me/6285806528052?text=Nama: ${nama}%0AEmail: ${email}%0APesan: ${pesan}`;
     window.open(url, '_blank');
 });
